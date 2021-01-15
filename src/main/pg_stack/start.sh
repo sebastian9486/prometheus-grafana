ad2616b8244b33209cda493aee4a11c438652999
sebastian <sommerfeld.sebastian@gmail.com>
2021-01-15 13:02:42 +0100

    added initial prometheus + grafana setup

---
+++ src/main/pg_stack/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# load common vars
+source vars.sh
+
+echo -e "$LOG_INFO starting containers"
+docker-compose up -d
+
+echo -e "$LOG_INFO ------------------------------------------------------------------"
+echo -e "$LOG_INFO startup in progress (detached)"
+echo -e "$LOG_INFO ------------------------------------------------------------------"
+
+echo -e "$LOG_INFO showing logs"
+docker-compose logs -f
---
+++ src/main/pg_stack/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# load common vars
+source vars.sh
+
+echo -e "$LOG_INFO shutting down"
+docker-compose down -v
+
+echo -e "$LOG_DONE ------------------------------------------------------------------"
+echo -e "$LOG_DONE stopped and removed containers, networks and volumes"
+echo -e "$LOG_DONE ------------------------------------------------------------------"
